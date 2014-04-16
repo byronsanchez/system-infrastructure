083bf8aac3e742e00a40c3fbbcf9d2f93cc5cce5
Byron Sanchez <byron@hackbytes.io>
2014-04-16 15:58:49 +0000

    Added email notification to be sourced at login
    Signed-off-by: Byron Sanchez <byron@hackbytes.com>

---
+++ files/base/etc/profile.d/simple-ids.custom.sh
@@ -0,0 +1,11 @@
+###########################################
+#                                         #
+# MANAGED BY PUPPET                       #
+#                                         #
+# Manual changes WILL be overwritten      #
+#                                         #
+###########################################
+
+# IDS for any logins
+pinky | mail -s "[nitelite.io] Login on ${HOSTNAME}" bfs5030+nitelite@gmail.com
+
