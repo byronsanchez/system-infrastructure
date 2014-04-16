d8056ed8d523beae5168574e835db0b8e400fbcf
Byron Sanchez <byron@hackbytes.io>
2014-04-16 19:09:49 +0000

    Added Android SDK tools to PATh
    Signed-off-by: Byron Sanchez <byron@hackbytes.com>

---
+++ files/mobile/etc/profile.d/path.custom.sh
@@ -0,0 +1,12 @@
+###########################################
+#                                         #
+# MANAGED BY PUPPET                       #
+#                                         #
+# Manual changes WILL be overwritten      #
+#                                         #
+###########################################
+
+PATH="/opt/android-sdk-update-manager/platform-tools/:${PATH}" 
+PATH="/opt/android-sdk-update-manager/tools/:${PATH}"
+
+export PATH
