18b4b3637e9ea3715a54113edf38c3340e15ea97
Byron Sanchez <byron@hackbytes.io>
2014-04-16 15:58:20 +0000

    Added fortune to be sourced at login
    Signed-off-by: Byron Sanchez <byron@hackbytes.com>

---
+++ files/base/etc/profile.d/fortune.custom.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+###########################################
+#                                         #
+# MANAGED BY PUPPET                       #
+#                                         #
+# Manual changes WILL be overwritten      #
+#                                         #
+###########################################
+
+fortune | cowsay
+
