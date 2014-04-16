94e3373e9958841d4be2515776e1f6c673d032a6
Byron Sanchez <byron@hackbytes.io>
2014-04-16 15:14:06 +0000

    Added definition for ANDROID_HOME variable
    Signed-off-by: Byron Sanchez <byron@hackbytes.com>

---
+++ files/mobile/etc/profile.d/mobile.custom.sh
@@ -0,0 +1,11 @@
+###########################################
+#                                         #
+# MANAGED BY PUPPET                       #
+#                                         #
+# Manual changes WILL be overwritten      #
+#                                         #
+###########################################
+
+ANDROID_HOME="/opt/android-sdk-update-manager"
+export ANDROID_HOME
+
