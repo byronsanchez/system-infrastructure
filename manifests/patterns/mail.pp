4c6610daff904a55fcd58f955c6254443f3944e5
Byron Sanchez <byron@hackbytes.io>
2014-04-17 01:32:30 +0000

    Added mail pattern
    Signed-off-by: Byron Sanchez <byron@hackbytes.com>

---
+++ manifests/patterns/mail.pp
@@ -0,0 +1,38 @@
+class mail {
+
+  file { "/etc/cron.daily/mirror_mail":
+    ensure => present,
+    owner  => "root",
+    group  => "root",
+    mode   => 0755,
+    path   => "/etc/cron.daily/mirror_mail",
+    source => "puppet:///files/mail/etc/cron.daily/mirror_mail",
+    require => File["/etc/cron.daily"],
+  }
+
+  file { "/etc/portage/package.use/mutt":
+    ensure => present,
+    owner => "root",
+    group => "root",
+    require => File['/etc/portage/package.use'],
+    path => "/etc/portage/package.use/mutt",
+    source => "puppet:///files/mail/etc/portage/package.use/mutt",
+  }
+
+  $packages = [
+    "offlineimap",
+    "msmtp",
+    "notmuch",
+    "urlview",
+    "mutt",
+    "dev-python/keyring",
+  ]
+
+  package { $packages:
+    ensure  => installed,
+    require => File['/etc/portage/package.use/mutt'],
+
+  }
+
+}
+
