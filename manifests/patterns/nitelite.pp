0b79394b97238980938081cb8ffe96373d96e97b
Byron Sanchez <byron@hackbytes.io>
2014-04-24 02:29:43 +0000

    Abstracted nginx website configurations into a module
    nl_nginx is now responsible of generating website configurations. By using a
    template, redundancy is eliminated!
    
    The nitelite pattern has been updated to reflect the change.
    
    Signed-off-by: Byron Sanchez <byron@hackbytes.com>

--- manifests/patterns/nitelite.pp
+++ manifests/patterns/nitelite.pp
@@ -2,54 +2,30 @@
 
 class nitelite($environment) {
 
-  # Configs based on environments
-  # staging, development or production
-
-  # You will need to create config files with the proper environment prefixing
-  # the domain name.
-  define website ($websiteName, $environmentName) {
-
-    if $environmentName == "production" {
-      $realWebsiteName = "${websiteName}"
-    }
-    else {
-      $realWebsiteName = "${environmentName}.${websiteName}"
-    }
-
-    file { "/etc/nginx/sites-available/${realWebsiteName}":
-      ensure => present,
-      owner  => "root",
-      group  => "root",
-      mode   => 0644,
-      path   => "/etc/nginx/sites-available/${realWebsiteName}",
-      source => "puppet:///files/nitelite/etc/nginx/sites-available/${realWebsiteName}",
-      require => File["/etc/nginx/sites-available"],
-    }
-
-    file { "/etc/nginx/sites-enabled/${realWebsiteName}":
-       ensure => "link",
-       target => "/etc/nginx/sites-available/${realWebsiteName}",
-    }
-  }
-
-  website { "hackbytes.com":
+  nl_nginx::website { "hackbytes.com":
     websiteName     => "hackbytes.com",
     environmentName => "${environment}",
+    feed_path       => "hackbytes",
+    root_path       => "/current/_site"
   }
 
-  website { "chompix.com":
+  nl_nginx::website { "chompix.com":
     websiteName     => "chompix.com",
     environmentName => "${environment}",
+    feed_path       => "byronsanchez",
   }
 
-  website { "nitelite.io":
+  nl_nginx::website { "nitelite.io":
     websiteName     => "nitelite.io",
     environmentName => "${environment}",
+    feed_path       => "nitelite",
+    root_path       => "/current/build"
   }
 
-  website { "tehpotatoking.com":
+  nl_nginx::website { "tehpotatoking.com":
     websiteName     => "tehpotatoking.com",
     environmentName => "${environment}",
+    feed_path       => "tehpotatoking",
   }
 
   file { "/etc/sudoers.d/deployer":
@@ -97,74 +73,12 @@ class nitelite($environment) {
     source => "puppet:///files/nitelite/etc/exim/system_filter.exim",
     require => File["/etc/exim"],
   }
-
-  # TODO: Install PECL YAML extension
-  # TODO: development vs production param for php.ini
-  file { "/etc/portage/package.use/php":
-    ensure => present,
-    owner => "root",
-    group => "root",
-    require => File['/etc/portage/package.use'],
-    path => "/etc/portage/package.use/php",
-    source => "puppet:///files/nitelite/etc/portage/package.use/php",
-  }
-
-  file { "/etc/php":
-    ensure => "directory",
-    owner => "root",
-    group => "root",
-  }
-
-  file { "/etc/php/fpm-php5.5":
-    ensure  => "directory",
-    owner   => "root",
-    group   => "root",
-    require => File["/etc/php"],
-  }
-
-  file { "/etc/php/fpm-php5.5/php-fpm.conf":
-    ensure => present,
-    owner => "root",
-    group => "root",
-    require => File['/etc/php/fpm-php5.5'],
-    path => "/etc/php/fpm-php5.5/php-fpm.conf",
-    source => "puppet:///files/nitelite/etc/php/fpm-php5.5/php-fpm.conf",
-  }
-
-  file { "/etc/php/fpm-php5.5/pool.d":
-    ensure  => "directory",
-    owner   => "root",
-    group   => "root",
-    require => File["/etc/php/fpm-php5.5"],
-  }
-
-  file { "/etc/php/fpm-php5.5/pool.d/www.conf":
-    ensure => present,
-    owner => "root",
-    group => "root",
-    require => File['/etc/php/fpm-php5.5/pool.d'],
-    path => "/etc/php/fpm-php5.5/pool.d/www.conf",
-    source => "puppet:///files/nitelite/etc/php/fpm-php5.5/pool.d/www.conf",
-  }
-
   $packages = [
     "nodejs",
-    "php",
-    "pecl-yaml",
     "exim",
     "sqlite3",
   ]
 
   package { $packages: ensure => installed }
-
-  service { 'php-fpm':
-    ensure => running,
-    enable => true,
-    subscribe => File['/etc/php/fpm-php5.5/php-fpm.conf'],
-    require   => [
-      File['/etc/php/fpm-php5.5/php-fpm.conf'],
-      Package[php],
-    ],
-  }
 
 }
---
+++ modules/nl_nginx/manifests/init.pp
@@ -0,0 +1,33 @@
+class nl_nginx {
+
+  # websiteName = domain name without environment appended to the front (eg.
+  # hackbytes.com)
+  #
+  # realWebsiteName = domain name WITH env appended to the front (eg.
+  # stage.hackbytes.com)
+  define website ($websiteName, $environmentName, $feed_path, $root_path = '') {
+
+    if $environmentName == "production" {
+      $realWebsiteName = "${websiteName}"
+    }
+    else {
+      $realWebsiteName = "${environmentName}.${websiteName}"
+    }
+
+    file { "/etc/nginx/sites-available/${realWebsiteName}":
+      ensure => present,
+      owner  => "root",
+      group  => "root",
+      mode   => 0644,
+      path   => "/etc/nginx/sites-available/${realWebsiteName}",
+      content => template("nl_nginx/etc/nginx/sites-available/website.erb"),
+      require => File["/etc/nginx/sites-available"],
+    }
+
+    file { "/etc/nginx/sites-enabled/${realWebsiteName}":
+       ensure => "link",
+       target => "/etc/nginx/sites-available/${realWebsiteName}",
+    }
+  }
+
+}
