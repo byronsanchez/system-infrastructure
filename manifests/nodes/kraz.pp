45f8dfd48844356c91446a0441ecd4062f6ed1b7
Byron Sanchez <byron@hackbytes.io>
2014-04-24 03:29:01 +0000

    Added proxy node, kraz
    Signed-off-by: Byron Sanchez <byron@hackbytes.com>

---
+++ manifests/nodes/kraz.pp
@@ -0,0 +1,37 @@
+node 'kraz.internal.nitelite.io' inherits network {
+
+  $iptables_type="proxy"
+  # Locale
+  $linguas="en_US en en_GB es zh_CN zh_TW zh_HK ja jp fr_FR fr fr_CA ru_RU ru"
+
+  # ldap
+  $ldap_type="client"
+
+  class { "base":
+    hostname          => "kraz",
+    network_interface => "eth0",
+  }
+  class { "gentoo": }
+  class { "security":
+    iptables_type => "${iptables_type}",
+  }
+  class { "ssh":
+    username => [
+      "rbackup",
+      "staff",
+    ],
+  }
+
+  # Add node-specific resources
+  class { "nasclient": }
+  class { "ldap":
+    ldap_type => "${ldap_type}"
+  }
+  class { "proxy": }
+
+  # users
+  class { "root": }
+  class { "rbackup": }
+  class { "staff": }
+
+}
