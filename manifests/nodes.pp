61fa843f6c54305a350c771b7c0d0b82a1fcdebd
Byron Sanchez <byron@hackbytes.io>
2014-04-24 02:14:37 +0000

    Updated global network variables
    Updated:
    
      - ldap server address
      - internal and external domain names
    
    Signed-off-by: Byron Sanchez <byron@hackbytes.com>

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -7,15 +7,17 @@ node "network" {
   # network settings
   # TODO: account for differences between the internal and external domains
   $domain = "internal.nitelite.io"
+  $internal_domain = "internal.nitelite.io"
+  $external_domain = "external.nitelite.io"
 
   # network services
   $puppetmaster_address = "10.66.77.100"
   $hypervisor_address = "10.66.77.100"
   $mediaserver_address = "10.66.77.100"
-  $ldapserver_address = "10.66.77.100"
   $binhost_address = "10.66.77.100"
   $ns1_address = "10.66.77.101"
   $ns2_address = "10.66.77.102"
+  $ldapserver_address = "10.66.77.103"
 
 }
 
