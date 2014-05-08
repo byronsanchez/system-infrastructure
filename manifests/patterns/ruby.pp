cda43ef4a10886de03626fd9da4c88430c5dfb41
Byron Sanchez <byron@hackbytes.io>
2014-05-08 22:14:02 +0000

    Added ruby compile-time dependencies to ruby pattern
    Signed-off-by: Byron Sanchez <byron@hackbytes.com>

--- manifests/patterns/ruby.pp
+++ manifests/patterns/ruby.pp
@@ -1,8 +1,22 @@
 class ruby {
 
-  # TODO: require nitelite overlays
   $packages = [
-    "dev-ruby/rvm",
+    # needed to compile rubies for rvm
+    "net-misc/curl",
+    "sys-devel/patch",
+    "app-shells/bash",
+    "virtual/libiconv",
+    "sys-libs/readline",
+    "sys-libs/zlib",
+    "dev-libs/openssl",
+    "dev-libs/libyaml",
+    "dev-db/sqlite",
+    "sys-devel/libtool",
+    "sys-devel/gcc",
+    "sys-devel/autoconf",
+    "sys-devel/automake",
+    "sys-devel/bison",
+    "sys-devel/m4",
   ]
 
   package { $packages:
