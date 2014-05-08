730968ad596df4231b0d37685f1040639053f7b0
Byron Sanchez <byron@hackbytes.io>
2014-05-08 20:50:23 +0000

    Configured workstation node, mira
    Signed-off-by: Byron Sanchez <byron@hackbytes.com>

---
+++ manifests/nodes/mira.pp
@@ -0,0 +1,76 @@
+node 'mira.internal.nitelite.io' inherits network {
+
+  class { "base":
+    hostname          => "mira",
+    network_interface => "eth0",
+  }
+
+  class { "gentoo":
+    use_flags     => "mysql",
+    linguas       => "en_US en en_GB es zh_CN zh_TW zh_HK ja jp fr_FR fr fr_CA ru_RU ru",
+    video_cards   => "",
+    input_devices => "evdev",
+    lowmemorybox  => false,
+  }
+
+  class { "security":
+    iptables_type => "workstation",
+  }
+
+  class { "ssh":
+    username => [
+      "root",
+      "rbackup",
+      "staff",
+      "byronsanchez",
+    ],
+  }
+
+  class { "nasclient": }
+
+  class { "ldap":
+    ldap_type => "client"
+  }
+
+  class { "pki":
+    ca_type => "client",
+  }
+
+  class { "vcs": }
+
+  class { "rsyncd": }
+
+  class { "webserver": }
+
+  class { "vpn":
+    vpn_type => "client",
+  }
+
+  class { "xorgserver": }
+
+  class { "media": }
+
+  class { "workstation": }
+
+  class { "nodejs": }
+
+  class { "php": }
+
+  class { "java": }
+
+  class { "mobile": }
+
+  # users
+  class { "root": }
+  class { "rbackup": }
+  class { "deployer": }
+  class { "staff": }
+  class { "byronsanchez":
+    #groups    => ['plugdev', 'android'],
+    groups => ['audio', 'cdrom', 'kvm', 'usb', 'wheel',],
+  }
+  class { "logger": }
+
+
+
+}
