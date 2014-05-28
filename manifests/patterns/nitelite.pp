43b3a97a088819bd20f0e6abd89408ab5f462bc3
Byron Sanchez <byron@hackbytes.io>
2014-05-28 04:10:55 +0000

    Updated website config options by adding php and rss parameters
    Signed-off-by: Byron Sanchez <byron@hackbytes.com>

--- manifests/patterns/nitelite.pp
+++ manifests/patterns/nitelite.pp
@@ -5,26 +5,34 @@ class nitelite($environment) {
   nl_nginx::website { "hackbytes.com":
     websiteName     => "hackbytes.com",
     environmentName => "${environment}",
+    root_path       => "/current/_site",
+    enable_php      => true,
+    enable_feed     => true,
     feed_path       => "hackbytes",
-    root_path       => "/current/_site"
   }
 
   nl_nginx::website { "chompix.com":
     websiteName     => "chompix.com",
     environmentName => "${environment}",
+    enable_php      => true,
+    enable_feed     => true,
     feed_path       => "byronsanchez",
   }
 
   nl_nginx::website { "nitelite.io":
     websiteName     => "nitelite.io",
     environmentName => "${environment}",
+    root_path       => "/current/build",
+    enable_php      => true,
+    enable_feed     => true,
     feed_path       => "nitelite",
-    root_path       => "/current/build"
   }
 
   nl_nginx::website { "tehpotatoking.com":
     websiteName     => "tehpotatoking.com",
     environmentName => "${environment}",
+    enable_php      => true,
+    enable_feed     => true,
     feed_path       => "tehpotatoking",
   }
 
