65c0a2897559bb45fbf6009099ba29facd46c1f4
Byron Sanchez <byron@hackbytes.io>
2014-05-30 16:56:56 +0000

    Removed nitelite-specific modules from modules directory

--- modules/nl_hosts/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class nl_hosts ($node_name = '') {
-  file { '/etc/hosts':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('nl_hosts/etc/hosts.erb'),
-  }
-}
