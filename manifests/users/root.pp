7e7a927d8fb285fd9dc804ed3ed52949be95d2c5
Byron Sanchez <byron@hackbytes.io>
2014-09-07 00:35:06 +0000

    Added root user password management via hiera

--- manifests/users/root.pp
+++ manifests/users/root.pp
@@ -1,3 +1,18 @@
 class root {
 
+  $passwords = hiera("passwords", "")
+
+  if $passwords {
+    $password = $passwords['root']
+  }
+
+  if $password {
+
+    user { 'root':
+      ensure   => 'present',
+      password => $password,
+    }
+
+  }
+
 }
