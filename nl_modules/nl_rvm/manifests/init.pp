adb0b82a43b08fefbb08de7a5dcbc4c402fe7fdd
Byron Sanchez <byron@hackbytes.io>
2015-03-21 20:23:23 +0000

    Update RVM module to perform complete RVM install

--- nl_modules/nl_rvm/manifests/init.pp
+++ nl_modules/nl_rvm/manifests/init.pp
@@ -7,7 +7,8 @@ class nl_rvm(
     cwd     => "${home}",
     # Install rvm-stable in the home directory without touching the dotfiles.
     # dotfiles are managed seperately (*rc and *profile)
-    command => "/usr/bin/curl -sSL https://get.rvm.io | bash -s stable -- --ignore-dotfiles",
+    # Removed the -- for zsh shells
+    command => "/usr/bin/gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3; /usr/bin/curl -sSL https://get.rvm.io | /bin/bash -s stable --ignore-dotfiles; /usr/bin/gem install rails",
     creates => "${home}/.rvm",
     user    => "${user}",
   }
