3cefa4675f795be15e3b20017a85a0a1cba5b05c
Byron Sanchez <byron@hackbytes.io>
2015-03-22 01:59:35 +0000

    Add template functionality to nl_files module

--- nl_modules/nl_files/manifests/init.pp
+++ nl_modules/nl_files/manifests/init.pp
@@ -4,13 +4,31 @@ define nl_files(
   $mode,
   $requires,
   $source,
+  $file_type = '',
 ) {
-  file { "$name":
-    ensure  => present,
-    path    => $name,
-    owner   => $owner,
-    group   => $group,
-    require => $requires,
-    source => "puppet:///files/${source}${name}",
+
+  if $file_type == "template" {
+
+    file { "$name":
+      ensure  => present,
+      path    => $name,
+      owner   => $owner,
+      group   => $group,
+      require => $requires,
+      content => template("${source}${name}.erb"),
+    }
+
+  }
+  else {
+
+    file { "$name":
+      ensure  => present,
+      path    => $name,
+      owner   => $owner,
+      group   => $group,
+      require => $requires,
+      source => "puppet:///files/${source}${name}",
+    }
+
   }
 }
