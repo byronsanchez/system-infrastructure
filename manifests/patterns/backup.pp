b4c0f41a186b96515fb57f39aedb00db01f8d51a
Byron Sanchez <byron@hackbytes.io>
2015-03-29 00:00:58 +0000

    Update backup pattern to support workstation backups
    - Convert rsnapshot cron job to template format to define workstation-specific 
      commands
    - Add new rsnapshot ignore rules
      - Video project directory
      - Linux trash directory
      - Unnecessary CI directories (these files can be regenerated from other data 
        that is already being backed up)
    - Add workstation-specific backup scripts
    - Remove osx backups

--- manifests/patterns/backup.pp
+++ manifests/patterns/backup.pp
@@ -3,26 +3,10 @@ class backup(
 ) {
 
   $nodes = hiera('nodes', '')
+  $backup_uuid = hiera('backup_uuid', '')
+  $backup_keyfile = hiera('backup_keyfile', '')
 
-  if $backup_type == "server" {
-
-    file { "/home/rbackup/.ssh/rbackup_rsa":
-      ensure => present,
-      owner => "rbackup",
-      group => "rbackup",
-      mode => 0600,
-      path => "/home/rbackup/.ssh/rbackup_rsa",
-      source => "puppet:///secure/ssh/rbackup_rsa",
-    }
-
-    file { "/home/rbackup/.ssh/rbackup_rsa.pub":
-      ensure => present,
-      owner => "rbackup",
-      group => "rbackup",
-      mode => 0644,
-      path => "/home/rbackup/.ssh/rbackup_rsa.pub",
-      source => "puppet:///secure/ssh/rbackup_rsa.pub",
-    }
+  if ($backup_type == "server") or ($backup_type == "workstation") {
 
     file { '/etc/rsnapshot.conf':
       ensure  => present,
@@ -45,20 +29,40 @@ class backup(
     file { '/etc/cron.d/rsnapshot':
       ensure  => present,
       path    => "/etc/cron.d/rsnapshot",
-      source  => 'puppet:///files/backup/etc/cron.d/rsnapshot',
+      content => template('backup/etc/cron.d/rsnapshot.erb'),
       group   => '0',
       owner   => '0',
       require => File['/etc/cron.d'],
     }
 
-    file { '/etc/cron.daily/backup_mirror':
-      ensure  => present,
-      path    => "/etc/cron.daily/backup_mirror",
-      source  => 'puppet:///files/backup/etc/cron.daily/backup_mirror',
-      group   => '0',
-      owner   => '0',
-      mode    => 0755,
-      require => File['/etc/cron.daily'],
+    $server_packages = [
+      "rsnapshot",
+    ]
+
+    package {
+      $server_packages: ensure => installed,
+    }
+
+  }
+
+  if $backup_type == "server" {
+
+    file { "/home/rbackup/.ssh/rbackup_rsa":
+      ensure => present,
+      owner => "rbackup",
+      group => "rbackup",
+      mode => 0600,
+      path => "/home/rbackup/.ssh/rbackup_rsa",
+      source => "puppet:///secure/ssh/rbackup_rsa",
+    }
+
+    file { "/home/rbackup/.ssh/rbackup_rsa.pub":
+      ensure => present,
+      owner => "rbackup",
+      group => "rbackup",
+      mode => 0644,
+      path => "/home/rbackup/.ssh/rbackup_rsa.pub",
+      source => "puppet:///secure/ssh/rbackup_rsa.pub",
     }
 
     file { '/usr/local/bin/backup-pgsql.sh':
@@ -97,32 +101,78 @@ class backup(
       mode    => '755',
     }
 
-    $server_packages = [
-      "rsnapshot",
-    ]
-
-    package {
-      $server_packages: ensure => installed,
+    file { '/etc/cron.daily/backup_mirror':
+      ensure  => present,
+      path    => "/etc/cron.daily/backup_mirror",
+      source  => 'puppet:///files/backup/etc/cron.daily/backup_mirror',
+      group   => '0',
+      owner   => '0',
+      mode    => 0755,
+      require => File['/etc/cron.daily'],
     }
 
   }
 
-  file { '/home/rbackup/validate-rsync.sh':
-    ensure  => present,
-    path => "/home/rbackup/validate-rsync.sh",
-    source => 'puppet:///files/backup/home/rbackup/validate-rsync.sh',
-    owner   => 'rbackup',
-    group   => 'rbackup',
-    mode    => '755',
+  elsif $backup_type == "workstation" {
+
+    file { '/usr/local/bin/backup-mirror.sh':
+      ensure  => present,
+      path    => "/usr/local/bin/backup-mirror.sh",
+      content => template('backup/usr/local/bin/backup-mirror.sh.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '755',
+    }
+
+    vcsrepo { "/var/lib/nitelite/workstation/backups":
+      ensure   => present,
+      provider => git,
+      source   => "https://github.com/byronsanchez/backups.git",
+      require  => File["/var/lib/nitelite/workstation"],
+    }
+
+    exec { "install_workstation_backup_scripts":
+      command => "/usr/local/bin/install-workstation-backup-scripts",
+      require => [
+        Vcsrepo["/var/lib/nitelite/workstation/backups"],
+        File["/usr/local/bin/install-workstation-backup-scripts"],
+      ],
+    }
+
+    file { '/usr/local/bin/install-workstation-backup-scripts':
+      ensure  => present,
+      path => "/usr/local/bin/install-workstation-backup-scripts",
+      source => 'puppet:///files/backup/usr/local/bin/install-workstation-backup-scripts',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '755',
+    }
+
   }
 
-  file { '/usr/bin/rsync_wrapper.sh':
-    ensure  => present,
-    path => "/usr/bin/rsync_wrapper.sh",
-    source => 'puppet:///files/backup/usr/bin/rsync_wrapper.sh',
-    owner   => 'rbackup',
-    group   => 'rbackup',
-    mode    => '755',
+  else {
+
+    # If no server type was specified, assume the node is a client whose
+    # contents will be backed up by a backup server.
+
+    file { '/home/rbackup/validate-rsync.sh':
+      ensure  => present,
+      path => "/home/rbackup/validate-rsync.sh",
+      source => 'puppet:///files/backup/home/rbackup/validate-rsync.sh',
+      owner   => 'rbackup',
+      group   => 'rbackup',
+      mode    => '755',
+    }
+
+    file { '/usr/bin/rsync_wrapper.sh':
+      ensure  => present,
+      path => "/usr/bin/rsync_wrapper.sh",
+      source => 'puppet:///files/backup/usr/bin/rsync_wrapper.sh',
+      owner   => 'rbackup',
+      group   => 'rbackup',
+      mode    => '755',
+    }
+
   }
 
   # every node gets a backup dir for dumpfiles. These files will then be
