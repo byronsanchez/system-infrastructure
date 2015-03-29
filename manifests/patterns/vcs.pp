aef46a6f882aa51c25e0d6352447008a8c9587db
Byron Sanchez <byron@hackbytes.io>
2015-03-29 15:44:47 +0000

    Update vcs pattern
    - Switch internal vcs server domain from fossil.nitelite.io to 
      fossil.internal.nitelite.io. This is to make fossil.nitnlite.io a public  
      mirror.
    - Remove repo mirror syncing cron jobs and replace them with scripts. Add a cron 
      job that runs every minute and pings the fossil repo for new commits. When new 
      commits exist, then changes are pushed to other servers. This allows for more 
      instantaneous public server pushes instead of nightly pushes, which is great 
      for overlay repos when new ebuilds must be pulled across nodes.
    - Set public repo mirrors to the tpk domain.
    - Split vcs.d into repo type configs. With the addition of a fossil mirror to 
      the already existing git syncs, syncing of both types must be handled 
      differently.

--- manifests/patterns/vcs.pp
+++ manifests/patterns/vcs.pp
@@ -1,8 +1,349 @@
+# server - the authoritative vcs server. local subnet access only
+# mirror - a host mirroring the repos. internet accessible
+# client - just installs client apps
 class vcs(
   $vcs_type = '',
+  $mirror_environment = '',
 ) {
 
+  $fossilpw = hiera('fossilpw', '')
+
   if $vcs_type == "server" {
+
+    file { "/etc/nitelite/vcs.d":
+      ensure => directory,
+      owner => "root",
+      group => "root",
+      require => File['/etc/nitelite'],
+    }
+
+    file { "/etc/nitelite/vcs.d/fossil":
+      ensure => directory,
+      owner => "root",
+      group => "root",
+      require => File['/etc/nitelite/vcs.d'],
+    }
+
+    file { "/etc/nitelite/vcs.d/fossil/byronsanchez":
+      ensure => directory,
+      owner => "root",
+      group => "root",
+      require => File['/etc/nitelite/vcs.d/fossil'],
+    }
+
+    file { "/etc/nitelite/vcs.d/git":
+      ensure => directory,
+      owner => "root",
+      group => "root",
+      require => File['/etc/nitelite/vcs.d'],
+    }
+
+    file { "/etc/nitelite/vcs.d/git/byronsanchez":
+      ensure => directory,
+      owner => "root",
+      group => "root",
+      require => File['/etc/nitelite/vcs.d/git'],
+    }
+
+    $fossil_mirror_configs_byronsanchez = [
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/dotfiles',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/coloring-book-android',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/coloring-book-ios',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/gentoo-bootmodder',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/gentoo-overlay-a',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/gentoo-overlay-b',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/gentoo-overlay-applications',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/gentoo-provision',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/hackbytes.com',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/tehpotatoking.com',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/system-infrastructure',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/wintersmith-articles-helper',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/wintersmith-handleize-helper',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/wintersmith-robotskirt',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/wintersmith-tag-pages',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/creepypasta-files-android',
+      '/etc/nitelite/vcs.d/fossil/byronsanchez/creepypasta-files-ios',
+    ]
+
+    $git_mirror_configs_byronsanchez = [
+      '/etc/nitelite/vcs.d/git/byronsanchez/dotfiles',
+      '/etc/nitelite/vcs.d/git/byronsanchez/coloring-book-android',
+      '/etc/nitelite/vcs.d/git/byronsanchez/coloring-book-ios',
+      '/etc/nitelite/vcs.d/git/byronsanchez/gentoo-bootmodder',
+      '/etc/nitelite/vcs.d/git/byronsanchez/gentoo-overlay-a',
+      '/etc/nitelite/vcs.d/git/byronsanchez/gentoo-overlay-b',
+      '/etc/nitelite/vcs.d/git/byronsanchez/gentoo-overlay-applications',
+      '/etc/nitelite/vcs.d/git/byronsanchez/gentoo-provision',
+      '/etc/nitelite/vcs.d/git/byronsanchez/hackbytes.com',
+      '/etc/nitelite/vcs.d/git/byronsanchez/tehpotatoking.com',
+      '/etc/nitelite/vcs.d/git/byronsanchez/system-infrastructure',
+      '/etc/nitelite/vcs.d/git/byronsanchez/wintersmith-articles-helper',
+      '/etc/nitelite/vcs.d/git/byronsanchez/wintersmith-handleize-helper',
+      '/etc/nitelite/vcs.d/git/byronsanchez/wintersmith-robotskirt',
+      '/etc/nitelite/vcs.d/git/byronsanchez/wintersmith-tag-pages',
+      '/etc/nitelite/vcs.d/git/byronsanchez/creepypasta-files-android',
+      '/etc/nitelite/vcs.d/git/byronsanchez/creepypasta-files-ios',
+    ]
+
+    # TODO: rewrite the git_push script to read a single config file instead of
+    # the way it's currently implemented (multiple config files in different org
+    # dirs)
+
+    nl_files { $fossil_mirror_configs_byronsanchez:
+      owner     => 'www-data',
+      group     => 'www-data',
+      mode      => 0600,
+      requires  => File["/etc/nitelite/vcs.d/fossil/byronsanchez"],
+      source    => 'vcs',
+      file_type => 'template',
+    }
+
+    nl_files { $git_mirror_configs_byronsanchez:
+      owner    => 'www-data',
+      group    => 'www-data',
+      mode     => 0644,
+      requires  => File["/etc/nitelite/vcs.d/git/byronsanchez"],
+      source => 'vcs',
+    }
+
+    file { "/root/.ssh/vcs_rsa":
+      ensure => present,
+      owner => "root",
+      group => "root",
+      mode => 0600,
+      path => "/root/.ssh/vcs_rsa",
+      source => "puppet:///secure/ssh/vcs_rsa",
+    }
+
+    file { "/root/.ssh/vcs_rsa.pub":
+      ensure => present,
+      owner => "root",
+      group => "root",
+      mode => 0644,
+      path => "/root/.ssh/vcs_rsa.pub",
+      source => "puppet:///secure/ssh/vcs_rsa.pub",
+    }
+
+    # git.internal.nitelite.io
+    # TODO: Make use of the install instead of source path that is currently 
+    # used by uwsgi configs
+
+    exec { "webapp_config_cgit":
+      command => "/usr/sbin/webapp-config -I -h git.${internal_domain} -d git cgit 0.10",
+      creates => "/srv/www/git.${internal_domain}/htdocs/cgit",
+      require => [
+        Package[cgit],
+        File['/srv/www'],
+        File['/etc/vhosts/webapp-config'],
+      ]
+    }
+
+    # cgit internal backend nginx for https
+    nl_nginx::website { "cgit_internal":
+      websiteName     => "cgit.internal.nitelite.io",
+      environmentName => "production",
+      feed_path       => "git",
+      root_path       => "/htdocs/cgit",
+      port            => "8081",
+      enable_custom_configs => true,
+      enable_cgi      => true,
+      cgi_server      => "cgit.internal.nitelite.io:3129",
+    }
+
+    file { "/etc/nginx/conf.d/nitelite/git.internal.nitelite.io":
+      ensure => present,
+      owner => "root",
+      group => "root",
+      require => File['/etc/nginx/conf.d/nitelite'],
+      path => "/etc/nginx/conf.d/nitelite/git.internal.nitelite.io",
+      source => "puppet:///files/vcs/etc/nginx/conf.d/nitelite/git.internal.nitelite.io",
+    }
+
+  }
+
+  if ($vcs_type == "mirror") {
+
+    # git.tehpotatoking.com
+
+    exec { "webapp_config_git_${environment}":
+      command => "/usr/sbin/webapp-config -I -h git.tehpotatoking.com -d git cgit 0.10",
+      creates => "/srv/www/git.tehpotatoking.com/htdocs/cgit",
+      require => [
+        Package[cgit],
+        File['/srv/www'],
+        File['/etc/vhosts/webapp-config'],
+      ]
+    }
+
+    if $mirror_environment == "production" {
+
+      # cgit mirror frontend nginx for https (backend http not needed since the cgit
+      # server and the http server are hosted on the same node)
+      nl_nginx::website { "git":
+        websiteName     => "git.tehpotatoking.com",
+        environmentName => "${mirror_environment}",
+        feed_path       => "git",
+        root_path       => "/htdocs",
+        enable_ssl      => true,
+        ssl_cert_path   => "/etc/ssl/tehpotatoking.com/cacert.pem",
+        ssl_key_path    => "/etc/ssl/tehpotatoking.com/private/cakey.pem.unencrypted",
+        enable_custom_configs => true,
+        enable_cgi      => true,
+        cgi_server      => "cgit.internal.nitelite.io:3129",
+      }
+
+      # fossil mirror frontend nginx for https (backend http not needed since the
+      # fossil server and the http server are hosted on the same node)
+      nl_nginx::website { "fossil":
+        websiteName     => "fossil.tehpotatoking.com",
+        environmentName => "${mirror_environment}",
+        feed_path       => "fossil",
+        root_path       => "/htdocs",
+        enable_custom_configs => true,
+        enable_ssl      => true,
+        enable_cgi      => true,
+        # cgi scripts sent here
+        cgi_server      => "fossil.tehpotatoking.com:3128",
+        ssl_cert_path   => "/etc/ssl/tehpotatoking.com/cacert.pem",
+        ssl_key_path    => "/etc/ssl/tehpotatoking.com/private/cakey.pem.unencrypted",
+        proxy_pass      => "http://fossilserver",
+        proxy_redirect  => "http://fossil.tehpotatoking.com:4545/ https://fossil.tehpotatoking.com/",
+        upstream        => "fossilserver",
+        # non-cgi scripts will be handled by the fossil server
+        upstream_server => "fossil.tehpotatoking.com:4545",
+      }
+
+    }
+    else {
+
+      # cgit mirror frontend nginx for https (backend http not needed since the cgit
+      # server and the http server are hosted on the same node)
+      nl_nginx::website { "git":
+        websiteName     => "git.tehpotatoking.com",
+        environmentName => "${mirror_environment}",
+        feed_path       => "git",
+        root_path       => "/htdocs",
+        enable_ssl      => true,
+        ssl_cert_path   => "/etc/ssl/tehpotatoking.com/cacert.pem",
+        ssl_key_path    => "/etc/ssl/tehpotatoking.com/private/cakey.pem.unencrypted",
+        enable_custom_configs => true,
+        enable_cgi      => true,
+        cgi_server      => "cgit.internal.nitelite.io:3129",
+      }
+
+      # fossil mirror frontend nginx for https (backend http not needed since the
+      # fossil server and the http server are hosted on the same node)
+      nl_nginx::website { "fossil":
+        websiteName     => "fossil.tehpotatoking.com",
+        environmentName => "${mirror_environment}",
+        feed_path       => "fossil",
+        root_path       => "/htdocs",
+        enable_custom_configs => true,
+        enable_ssl      => true,
+        enable_cgi      => true,
+        # cgi scripts sent here
+        cgi_server      => "${mirror_environment}.fossil.tehpotatoking.com:3128",
+        ssl_cert_path   => "/etc/ssl/tehpotatoking.com/cacert.pem",
+        ssl_key_path    => "/etc/ssl/tehpotatoking.com/private/cakey.pem.unencrypted",
+        proxy_pass      => "http://fossilserver",
+        proxy_redirect  => "http://${mirror_environment}.fossil.tehpotatoking.com:4545/ https://${mirror_environment}.fossil.tehpotatoking.com/",
+        upstream        => "fossilserver",
+        # non-cgi scripts will be handled by the fossil server
+        upstream_server => "${mirror_environment}.fossil.tehpotatoking.com:4545",
+      }
+
+    }
+
+    # the front end server
+    file { "/etc/nginx/conf.d/nitelite/fossil.tehpotatoking.com":
+      ensure => present,
+      owner => "root",
+      group => "root",
+      require => File['/etc/nginx/conf.d/nitelite'],
+      path => "/etc/nginx/conf.d/nitelite/fossil.tehpotatoking.com",
+      source => "puppet:///files/vcs/etc/nginx/conf.d/nitelite/fossil.tehpotatoking.com",
+    }
+
+    # needs to be on the cgi server
+    file { "/etc/nginx/conf.d/nitelite/git.tehpotatoking.com":
+      ensure => present,
+      owner => "root",
+      group => "root",
+      require => File['/etc/nginx/conf.d/nitelite'],
+      path => "/etc/nginx/conf.d/nitelite/git.tehpotatoking.com",
+      source => "puppet:///files/vcs/etc/nginx/conf.d/nitelite/git.tehpotatoking.com",
+    }
+
+  }
+
+  if ($vcs_type == "server") or ($vcs_type == "mirror") {
+
+    # needs to be on the cgi server
+    file { "/etc/portage/package.accept_keywords/uwsgi":
+      ensure => present,
+      owner => "root",
+      group => "root",
+      require => File['/etc/portage/package.accept_keywords'],
+      path => "/etc/portage/package.accept_keywords/uwsgi",
+      source => "puppet:///files/vcs/etc/portage/package.accept_keywords/uwsgi",
+    }
+
+    file { "/etc/cron.d/fossil-ping":
+      ensure => present,
+      owner  => "root",
+      group  => "root",
+      mode   => 0755,
+      path   => "/etc/cron.d/fossil-ping",
+      source => "puppet:///files/vcs/etc/cron.d/fossil-ping",
+      require => File["/etc/cron.d"],
+    }
+
+    file { "/usr/local/bin/fossil-cli":
+      ensure => present,
+      owner  => "root",
+      group  => "root",
+      mode   => 0755,
+      path   => "/usr/local/bin/fossil-cli",
+      source => "puppet:///files/vcs/usr/local/bin/fossil-cli",
+    }
+
+    file { "/usr/local/bin/fossil-ping":
+      ensure => present,
+      owner  => "root",
+      group  => "root",
+      mode   => 0755,
+      path   => "/usr/local/bin/fossil-ping",
+      source => "puppet:///files/vcs/usr/local/bin/fossil-ping",
+    }
+
+    file { "/usr/local/bin/fossil-to-git-sync":
+      ensure => present,
+      owner  => "root",
+      group  => "root",
+      mode   => 0755,
+      path   => "/usr/local/bin/fossil-to-git-sync",
+      source => "puppet:///files/vcs/usr/local/bin/fossil-to-git-sync",
+    }
+
+    file { "/usr/local/bin/fossil-sync":
+      ensure => present,
+      owner  => "root",
+      group  => "root",
+      mode   => 0755,
+      path   => "/usr/local/bin/fossil-sync",
+      source => "puppet:///files/vcs/usr/local/bin/fossil-sync",
+    }
+
+    file { "/usr/local/bin/git-sync":
+      ensure => present,
+      owner  => "root",
+      group  => "root",
+      mode   => 0755,
+      path   => "/usr/local/bin/git-sync",
+      source => "puppet:///files/vcs/usr/local/bin/git-sync",
+    }
+
+    # FOSSIL
 
     file { "/srv/fossil/cgi-bin":
       ensure  => directory,
@@ -74,8 +415,6 @@ class vcs(
       require => File['/srv/fossil'],
     }
 
-    # FOSSIL
-
     file { "/srv/fossil":
       ensure => directory,
       owner => "root",
@@ -126,7 +465,7 @@ class vcs(
        target => '/etc/init.d/uwsgi',
     }
 
-    # CGIT
+    # GIT
 
     file { "/srv/git":
       ensure => directory,
@@ -194,81 +533,6 @@ class vcs(
        target => '/etc/init.d/uwsgi',
     }
 
-    file { "/etc/nitelite/vcs.d":
-      ensure => directory,
-      owner => "root",
-      group => "root",
-      require => File['/etc/nitelite'],
-    }
-
-    file { "/etc/cron.daily/fossil_to_git_sync":
-      ensure => present,
-      owner  => "root",
-      group  => "root",
-      mode   => 0755,
-      path   => "/etc/cron.daily/fossil_to_git_sync",
-      source => "puppet:///files/vcs/etc/cron.daily/fossil_to_git_sync",
-      require => File["/etc/cron.daily"],
-    }
-
-    file { "/etc/cron.daily/git_push":
-      ensure => present,
-      owner  => "root",
-      group  => "root",
-      mode   => 0755,
-      path   => "/etc/cron.daily/git_push",
-      source => "puppet:///files/vcs/etc/cron.daily/git_push",
-      require => File["/etc/cron.daily"],
-    }
-
-    file { "/usr/local/bin/fossil-cli":
-      ensure => present,
-      owner  => "root",
-      group  => "root",
-      mode   => 0755,
-      path   => "/usr/local/bin/fossil-cli",
-      source => "puppet:///files/vcs/usr/local/bin/fossil-cli",
-    }
-
-    file { "/etc/nitelite/vcs.d/byronsanchez":
-      ensure => directory,
-      owner => "root",
-      group => "root",
-      require => File['/etc/nitelite/vcs.d'],
-    }
-
-    $vcs_source_files_byronsanchez = [
-      '/etc/nitelite/vcs.d/byronsanchez/dotfiles',
-      '/etc/nitelite/vcs.d/byronsanchez/coloring-book-android',
-      '/etc/nitelite/vcs.d/byronsanchez/coloring-book-ios',
-      '/etc/nitelite/vcs.d/byronsanchez/gentoo-bootmodder',
-      '/etc/nitelite/vcs.d/byronsanchez/gentoo-overlay-a',
-      '/etc/nitelite/vcs.d/byronsanchez/gentoo-overlay-b',
-      '/etc/nitelite/vcs.d/byronsanchez/gentoo-overlay-applications',
-      '/etc/nitelite/vcs.d/byronsanchez/gentoo-provision',
-      '/etc/nitelite/vcs.d/byronsanchez/hackbytes.com',
-      '/etc/nitelite/vcs.d/byronsanchez/tehpotatoking.com',
-      '/etc/nitelite/vcs.d/byronsanchez/system-infrastructure',
-      '/etc/nitelite/vcs.d/byronsanchez/wintersmith-articles-helper',
-      '/etc/nitelite/vcs.d/byronsanchez/wintersmith-handleize-helper',
-      '/etc/nitelite/vcs.d/byronsanchez/wintersmith-robotskirt',
-      '/etc/nitelite/vcs.d/byronsanchez/wintersmith-tag-pages',
-      '/etc/nitelite/vcs.d/tehpotatoking/creepypasta-files-android',
-      '/etc/nitelite/vcs.d/tehpotatoking/creepypasta-files-ios',
-    ]
-
-    # TODO: rewrite the git_push script to read a single config file instead of
-    # the way it's currently implemented (multiple config files in different org
-    # dirs)
-
-    nl_files { $vcs_source_files_byronsanchez:
-      owner    => 'root',
-      group    => 'root',
-      mode     => 0644,
-      requires  => File["/etc/nitelite/vcs.d/byronsanchez"],
-      source => 'vcs',
-    }
-
     file { "/etc/portage/package.use/uwsgi":
       ensure => present,
       owner => "root",
@@ -284,7 +548,7 @@ class vcs(
       group   => "root",
       mode    => 0644,
       path    => "/etc/xinetd.d/fossil",
-      source  => "puppet:///files/vcs/etc/xinetd.d/fossil",
+      content => template("vcs/etc/xinetd.d/fossil.erb"),
       require => File["/etc/xinetd.d"],
     }
 
@@ -303,31 +567,6 @@ class vcs(
       owner => "root",
       group => "root",
       require => File['/var/lib/nitelite'],
-    }
-
-    file { "/var/lib/nitelite/vcs/repositories":
-      ensure => directory,
-      owner => "root",
-      group => "root",
-      require => File['/var/lib/nitelite/vcs'],
-    }
-
-    file { "/root/.ssh/vcs_rsa":
-      ensure => present,
-      owner => "root",
-      group => "root",
-      mode => 0600,
-      path => "/root/.ssh/vcs_rsa",
-      source => "puppet:///secure/ssh/vcs_rsa",
-    }
-
-    file { "/root/.ssh/vcs_rsa.pub":
-      ensure => present,
-      owner => "root",
-      group => "root",
-      mode => 0644,
-      path => "/root/.ssh/vcs_rsa.pub",
-      source => "puppet:///secure/ssh/vcs_rsa.pub",
     }
 
     $server_packages = [
@@ -359,36 +598,6 @@ class vcs(
         File['/etc/init.d/uwsgi.cgit'],
         Package[uwsgi],
       ],
-    }
-
-    exec { "webapp_config_cgit":
-      command => "/usr/sbin/webapp-config -I -h cgit.${internal_domain} -d cgit cgit 0.10",
-      creates => "/srv/www/cgit.${internal_domain}/htdocs/cgit",
-      require => [
-        Package[cgit],
-        File['/srv/www'],
-        File['/etc/vhosts/webapp-config'],
-      ]
-    }
-
-    nl_nginx::website { "cgit":
-      websiteName     => "cgit.internal.nitelite.io",
-      environmentName => "production",
-      feed_path       => "cgit",
-      root_path       => "/htdocs/cgit",
-      port            => "8081",
-      enable_custom_configs => true,
-      enable_cgi      => true,
-      cgi_server      => "cgit.internal.nitelite.io:3129",
-    }
-
-    file { "/etc/nginx/conf.d/nitelite/cgit.internal.nitelite.io":
-      ensure => present,
-      owner => "root",
-      group => "root",
-      require => File['/etc/nginx/conf.d/nitelite'],
-      path => "/etc/nginx/conf.d/nitelite/cgit.internal.nitelite.io",
-      source => "puppet:///files/vcs/etc/nginx/conf.d/nitelite/cgit.internal.nitelite.io",
     }
 
   }
