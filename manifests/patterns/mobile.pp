2ec30bbca18ab1970eb66204bd0c62e0f4dd29bc
Byron Sanchez <byron@hackbytes.io>
2015-12-26 03:59:28 +0000

    Remove outdated path variable scripts
    These scripts were used when bash was the primary workstation shell. These 
    variables should be set or updated in a user's local zsh init scripts.

--- manifests/patterns/mobile.pp
+++ manifests/patterns/mobile.pp
@@ -1,25 +1,5 @@
 class mobile {
 
-  # TODO: Update these to fit zsh
-
-  file { "/etc/profile.d/mobile.custom.sh":
-    ensure => present,
-    owner => "root",
-    group => "root",
-    require => File['/etc/profile.d'],
-    path => "/etc/profile.d/mobile.custom.sh",
-    source => "puppet:///files/mobile/etc/profile.d/mobile.custom.sh",
-  }
-
-  file { "/etc/profile.d/path.custom.sh":
-    ensure => present,
-    owner => "root",
-    group => "root",
-    require => File['/etc/profile.d'],
-    path => "/etc/profile.d/path.custom.sh",
-    source => "puppet:///files/mobile/etc/profile.d/path.custom.sh",
-  }
-
   $packages = [
     "android-sdk-update-manager",
     "ant",
