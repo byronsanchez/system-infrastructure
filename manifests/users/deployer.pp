699947c8fc8fdad882773614ba356fd8d46b2282
Byron Sanchez <byron@hackbytes.io>
2015-12-30 01:12:50 +0000

    Make sure user deployer has a password

--- manifests/users/deployer.pp
+++ manifests/users/deployer.pp
@@ -1,17 +1,29 @@
 class deployer {
 
-  user { 'deployer':
-    ensure           => 'present',
-    managehome => true,
-    gid              => 'www-data',
-    home             => '/home/deployer',
-    shell            => '/bin/sh',
-    uid              => '1002',
+  $passwords = hiera("passwords", "")
+
+  if $passwords {
+    $password = $passwords['deployer']
   }
 
-  group { 'deployer':
-    ensure => 'present',
-    gid  => '1002',
+  if $password {
+
+    user { 'deployer':
+      ensure     => 'present',
+      managehome => true,
+      gid        => 'www-data',
+      home       => '/home/deployer',
+      shell      => '/bin/sh',
+      uid        => '1002',
+      password   => $password,
+    }
+
+    group { 'deployer':
+      ensure => 'present',
+      gid  => '1002',
+    }
+
   }
 
 }
+
